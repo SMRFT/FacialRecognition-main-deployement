fab0778fd440426f2b0f9a6ecbad4717e74164e3
smrft <smrft@smrft.org>
2023-05-22 16:05:13 +0530

    deployement

--- Attendance_Management/settings.py
+++ Attendance_Management/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5#y-^bdkqu5-_2ttnr7(^*ai-i$2nh+ef)7+t$+%+hhol59h@!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -136,10 +136,12 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = BASE_DIR.joinpath("staticfiles")
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-
+STATICFILES_DIRS = [
+    BASE_DIR.joinpath('static')
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Attendance_Management.wsgi:application --log-file - --log-level debug

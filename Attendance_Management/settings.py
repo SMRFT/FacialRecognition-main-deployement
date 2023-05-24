7666b2788efc3fef4bffcb7f26d9a62b1b09576c
SMRFT <110091550+SMRFT@users.noreply.github.com>
2023-05-24 09:16:13 +0530

    Update settings.py

--- Attendance_Management/settings.py
+++ Attendance_Management/settings.py
@@ -30,7 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    # 'rest_framework_simplejwt',
+    'AttendanceApp.apps.AttendanceappConfig',
     'corsheaders',
     'AttendanceApp',
 
